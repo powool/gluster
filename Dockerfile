9f205cfcce888888668e6453fc70c7518288533d
Paul H Anderson <panderson@barracuda.com>
2018-03-07 11:12:52 -0500

    Cleanup, but mainly we now use gluster.org prebuilt binaries
    debian, ubuntu and centos docker images all run fairly old
    versions of gluster FS by default.
    
    So, we'll pull the latest build from gluster.org instead.
    
    One tricky part was we need to include the packge apt-transport-https,
    otherwise, we fail to do an https download via apt-get.
    
    Ubuntu appears not to package apt-transport-https independently,
    rather it uses a symlink to point to the http protocol in
    /usr/lib/apt/methods.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,25 @@
+# you can also use 'FROM debian'
 FROM php:cli
+# use up to date gluster.org built packages
 RUN apt-get update
+
+# this is key - you can't install gluster packages without it:
+RUN apt-get --assume-yes install apt-transport-https
+RUN apt-get --assume-yes install wget gnupg gnupg1 gnupg2
+RUN wget -O - http://download.gluster.org/pub/gluster/glusterfs/3.13/rsa.pub | apt-key add -
+RUN echo deb [arch=amd64] https://download.gluster.org/pub/gluster/glusterfs/3.13/LATEST/Debian/stretch/amd64/apt stretch main > /etc/apt/sources.list.d/gluster.list
+RUN apt-get update
+
+# now install gluster fs client code and a few other fun things
 RUN apt-get --assume-yes install glusterfs-client
 RUN apt-get --assume-yes install iputils-ping
 RUN apt-get --assume-yes install iproute2
 RUN apt-get --assume-yes install strace
-# RUN apt-get --assume-yes install fuse
-RUN mkdir -v /etc/glusterfs
+
+# setup to mount gluster fs
+RUN mkdir -pv /etc/glusterfs
 COPY fstab /etc/fstab
 COPY dockerstore.vol /etc/glusterfs
+
+# copy our test php file in
 COPY sqlite_tester_flock.php /usr/bin
--- test.sh
+++ test.sh
@@ -1,4 +1,40 @@
 #!/bin/bash
+
+if \! mountpoint /mnt ; then
+	echo "If you haven't already, make sure you have this line in fstab:"
+	echo "/etc/glusterfs/dockerstore.vol /mnt glusterfs rw,noauto 0 0"
+	echo "and also have the file /etc/glusterfs/dockerstore.vol with the contents:"
+	cat <<EOF
+volume remote1
+type protocol/client
+option transport-type tcp
+option remote-host 172.18.0.3
+option remote-subvolume /data/glusterfs/store/dockerstore
+end-volume
+
+volume remote2
+type protocol/client
+option transport-type tcp
+option remote-host 172.18.0.4
+option remote-subvolume /data/glusterfs/store/dockerstore
+end-volume
+
+volume remote3
+type protocol/client
+option transport-type tcp
+option remote-host 172.18.0.2
+option remote-subvolume /data/glusterfs/store/dockerstore
+end-volume
+
+volume replicate
+type cluster/replicate
+subvolumes remote1 remote2 remote3
+end-volume
+EOF
+	echo "Mounting gluster in /mnt"
+	sudo mount /mnt
+fi
+
 echo "force test container to be nice and fresh."
 docker build -t sqlite_test_container .
 
@@ -24,6 +60,8 @@ wait
 sqlite3 /mnt/testfile.db 'PRAGMA integrity_check'
 
 result=$(sqlite3 /mnt/testfile.db 'PRAGMA integrity_check')
-if [ "$result" != "ok" ] ; then
-	echo "uh oh, bad database!!!" | lolcat
+if [ "$result" = "ok" ] ; then
+	echo "yay, database checks out as ok!"
+else
+	echo "uh oh, bad database!!!"
 fi
